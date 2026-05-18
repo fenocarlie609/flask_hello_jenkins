0702d0c97430a87b2f9c00bb9f9455c37eafd9e1
fenocarlie609 <fenocarlie69@gmail.com>
2026-05-18 08:07:21 +0300

    Fix: restaurer app.py original

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/hello/')
 def hello_world():
-    return 'Hello World!\n Test auto deploy with Jenkins and Kubernetes'
+    return 'Hello World!\n'
 
 @app.route('/hello/<username>')
 def hello_user(username):
